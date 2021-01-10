0dedf7dc6c315c4b73069337b41438d61e747240
Armando <armando.gavrila@gmail.ro>
2021-01-10 16:03:11 +0200

    added dropzone in upload modal

--- assets/image_board/components/Posts/Upload/Upload.js
+++ assets/image_board/components/Posts/Upload/Upload.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Modal from "../../UI/Modal/Modal";
+import Dropzone from 'react-dropzone'
+import Input from '../../UI/Input/Input';
+import Button from '../../UI/Button/Button';
+import classes from './Upload.css';
 
-const upload = props => {
-    return (
-        <Modal show={props.show} modalClosed={props.close}>
-            dsa
-        </Modal>
-    );
+class Upload extends Component {
+    state = {
+        file: null
+    }
+
+    render() {
+        return (
+            <Modal show={this.props.show} modalClosed={this.props.close}>
+                <div className={classes.UploadForm}>
+                    <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                        {({getRootProps, getInputProps}) => (
+                            <div className={classes.DropZone} {...getRootProps()}>
+                                <h1>Upload a new post</h1>
+                                <input {...getInputProps()} />
+                                <div>Drag here</div>
+                            </div>
+                        )}
+                    </Dropzone>
+                    <Input type='text' placeholder={"Type in the caption"}/>
+                    <Input type='text' placeholder={"Type in the tags"}/>
+                    <Button>Upload Post</Button>
+                </div>
+            </Modal>
+        );
+    }
 }
 
-export default upload;
+export default Upload;
