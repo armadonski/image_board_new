76097fa49258a0ec8cc8df09ea30f3ea27080035
Armando <armando.gavrila@gmail.ro>
2021-01-11 18:29:44 +0200

    back to main page button in post page

--- assets/image_board/components/Posts/PostPage/PostPage.js
+++ assets/image_board/components/Posts/PostPage/PostPage.js
@@ -1,26 +1,13 @@
 import React, {Component} from 'react';
 import classes from './PostPage.css';
 import PostToolbar from '../../Navigation/Toolbar/PostToolbar/PostToolbar';
+import BackToIndex from '../../Navigation/BackToIndex';
 
 class PostPage extends Component {
     render() {
         return (
             <div className={classes.PostPage}>
-                <div>
-                    {this.props.post.caption}
-                   <PostToolbar clicked={this.props.navigation}/>
-                </div>
-                <div>
-                    <img className={classes.PostImage} alt={this.props.post.caption} src={this.props.post.image}/>
-                </div>
-                <div>
-                </div>
-                <div>
-                    Description
-                </div>
-                <div>
-                    Comments
-                </div>
+                <BackToIndex/>
             </div>
         );
     }
